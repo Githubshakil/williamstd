afdc15381158336ff47eb875ea40c5731eff5f01
Githubshakil <shakilmahmud007@gmail.com>
2025-03-14 22:55:25 +0600

    update

--- tailwindv3/src/components/layouts/Navbar.jsx
+++ tailwindv3/src/components/layouts/Navbar.jsx
@@ -4,6 +4,8 @@ const Navbar = () => {
   return (
     <nav>
         <div className='bg-red-500'>home</div>
+        <h1 className='text-2xl'>hello world</h1>
+        <p>hello world</p>
     </nav>
   )
 }
